6ebfbd70815fc420dedcee72385f16ebc55063a5
diegovargasg <diegodevelopero@gmail.com>
2020-06-18 22:22:42 +0200

    added logout method

--- src/App.js
+++ src/App.js
@@ -9,14 +9,36 @@ import RateViews from "./scenes/RateViews/";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { createBrowserHistory } from "history";
 import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
 import { withAuthenticator } from "@aws-amplify/ui-react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Auth } from "aws-amplify";
 
 function App() {
   const history = createBrowserHistory();
+  const style = { color: "white" };
+
+  const handleLogout = async () => {
+    try {
+      await Auth.signOut();
+      window.location.reload(false);
+    } catch (error) {
+      console.log("error signing out: ", error);
+    }
+  };
 
   return (
     <div className="App">
+      <Navbar bg="dark" expand="lg">
+        <Navbar.Brand className="col">
+          <span style={style}>Vanalytics</span>
+        </Navbar.Brand>
+        <div className="col text-right">
+          <Button variant="light" onClick={handleLogout}>
+            Logout
+          </Button>
+        </div>
+      </Navbar>
       <Container fluid className="mt-5">
         <Router history={history}>
           <Switch>
